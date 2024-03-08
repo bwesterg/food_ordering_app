bca2bc1f61a479512bfc22d76315519c0f07ae8d
bwesterg <benton.westergaard@gmail.com>
2024-03-08 12:53:12 -0700

    successfully authenticates new user in MongoDB database using Auth0.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,26 @@ import './global.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from './AppRoutes';
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      //^^ useful in dev environment to prevent requeries 
+      //when user clicks away from window
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient} >
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 )
